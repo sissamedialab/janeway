427519004fc11f8b6b9f5bf1a13441a94ba2cce7
Iacopo Spalletti <i.spalletti@nephila.it>
2023-12-31 19:13:06 +0100

    Allow to set TECHNICAL_REVISION

--- src/review/const.py
+++ src/review/const.py
@@ -10,6 +10,7 @@ class EditorialDecisions(EnumContains):
     UNDECLINE = 'undecline'
     MINOR_REVISIONS = 'minor_revisions'
     MAJOR_REVISIONS = 'major_revisions'
+    TECHNICAL_REVISION = "techRevision"
 
 
 class ReviewerDecisions(EnumContains):
