8b2a8fa0a9e089e3e11e5d855a699630329e42f8
Iacopo Spalletti <i.spalletti@nephila.it>
2024-09-10 23:27:00 +0200

    Add events for upload / delete files during the submission process

--- src/submission/views.py
+++ src/submission/views.py
@@ -588,6 +588,17 @@ def submit_files(request, article_id):
                 messages.WARNING,
                 _('File deleted'),
             )
+
+            event_logic.Events.raise_event(
+                event_logic.Events.ON_ARTICLE_FILE_DELETE,
+                **{
+                    'request': request,
+                    'file_id': file_id,
+                    'original_filename': file.original_filename,
+                    'article': article
+                }
+            )
+
             return redirect(reverse('submit_files', kwargs={'article_id': article_id}))
 
         if 'manuscript' in request.POST:
@@ -604,6 +615,18 @@ def submit_files(request, article_id):
                     new_file.label = form.cleaned_data['label']
                     new_file.description = form.cleaned_data['description']
                     new_file.save()
+
+                    event_logic.Events.raise_event(
+                        event_logic.Events.ON_ARTICLE_FILE_UPLOAD,
+                        **{
+                            'request': request,
+                            'file_id': new_file,
+                            'original_filename': new_file.original_filename,
+                            'file_type': 'manuscript',
+                            'article': article
+                        }
+                    )
+
                     return redirect(
                         reverse('submit_files', kwargs={'article_id': article_id}),
                     )
@@ -625,6 +648,18 @@ def submit_files(request, article_id):
                     new_file.label = form.cleaned_data['label']
                     new_file.description = form.cleaned_data['description']
                     new_file.save()
+
+                    event_logic.Events.raise_event(
+                        event_logic.Events.ON_ARTICLE_FILE_UPLOAD,
+                        **{
+                            'request': request,
+                            'file_id': new_file,
+                            'original_filename': new_file.original_filename,
+                            'file_type': 'data',
+                            'article': article
+                        }
+                    )
+
                     return redirect(reverse('submit_files', kwargs={'article_id': article_id}))
                 else:
                     modal = 'data'
