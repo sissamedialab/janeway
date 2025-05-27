f1ef3a5428e8e828b2cc84b60a5e8fff2365e139
Lorenzo Gramaccioni <l.gramaccioni@nephila.digital>
2025-05-27 15:07:06 +0200

    remove not needed setting

--- src/core/janeway_global_settings.py
+++ src/core/janeway_global_settings.py
@@ -682,6 +682,3 @@ TINYMCE_DEFAULT_CONFIG = {
         "| a11ycheck ltr rtl | code",
     **TINYMCE_CLIPBOARD_CLEANER,
 }
-
-# Enable hierarchical structure for keywords during submission.
-ENABLE_HIERARCHICAL_KEYWORDS = False
