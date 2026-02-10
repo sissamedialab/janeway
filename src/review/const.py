fd17d8ad2051d725f2acb5b5635d0a6519a10426
Iacopo Spalletti <i.spalletti@nephila.it>
2026-02-10 22:06:47 +0100

    feat: Add OPEN_APPEAL EditorialDecisions

--- src/review/const.py
+++ src/review/const.py
@@ -14,6 +14,8 @@ class EditorialDecisions(EnumContains):
     CONDITIONAL_ACCEPT = "conditional_accept"
     REVIEW = "review"
     TECHNICAL_REVISIONS = "tech_revisions"
+    OPEN_APPEAL = 'open_appeal'
+
 
 class ReviewerDecisions(EnumContains):
     DECISION_ACCEPT = "accept"
